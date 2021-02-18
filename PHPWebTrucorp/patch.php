d6b68319871ba7b5c18fca82b8c82f524eb7c40f
lClairvoyance <william.eka@hotmail.com>
2021-02-18 07:29:37 -0800

    commit

---
+++ PHPWebTrucorp/patch.php
@@ -0,0 +1,19 @@
+<?php
+$link = mysqli_connect("172.19.0.2","root","asd","trucorp");
+if (!$link){
+echo "tidak bisa konek ke MySQL" . PHP_EOL;
+exit;
+}
+
+$query = "SELECT * FROM Users";
+
+$result = mysqli_query($link,$query);
+
+$user = mysqli_num_rows($result);
+
+echo "There are ".$user" user ";
+
+mysqli_close($link);
+?>
+
+
---
+++ SQLTrucorp/users.sql
@@ -0,0 +1,10 @@
+CREATE  TABLE users{
+	ID int(12) PRIMARY KEY;
+	Nama varchar(50) ;
+	Kantor varchar(50);
+}
+
+INSERT INTO users (ID , Nama ,Kantor) VALUES (1,'Andi','Pusat');
+INSERT INTO users (ID , Nama ,Kantor) VALUES (2,'Budi','Cabang');
+INSERT INTO users (ID , Nama ,Kantor) VALUES (3,'Charlie','Pusat');
+
